3e66b03751b206a9dfe31789248e18acf3033032
ksanchez <addonis365@gmail.com>
2018-08-23 10:29:46 -0400

    Updates

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
-  res.render('index', { title: 'Inventario Addonis' });
-});
+    res.render('index', { title: 'Inventario' });
+  });
+
 
 function ensureAuthenticated(req, res, next){
   if(req.isAuthenticated()){
